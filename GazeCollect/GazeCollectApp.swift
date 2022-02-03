259c4eefeb247a3433b64d39bf1aac4b54bcec87
Janmayeneric <43655740+Janmayeneric@users.noreply.github.com>
2022-02-03 02:34:15 +0000

    Initial Commit

---
+++ GazeCollect/GazeCollectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeCollectApp.swift
+//  GazeCollect
+//
+//  Created by 胡欣宇 on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct GazeCollectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
